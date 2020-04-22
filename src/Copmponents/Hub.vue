70beaa58caf039bc1281e3b23379104327b58ec9
ErikRosberg00 <erik.rosberg@fsd.se>
2020-04-22 09:10:54 +0200

    rensat onödig text ifrån Hub.vue

--- src/Copmponents/Hub.vue
+++ src/Copmponents/Hub.vue
@@ -1,35 +1,19 @@
 <template>
 <div class="body">
 
-<div id="mySidenav" class="sidenav">
-  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-  <a href="#">About</a>
-  <a href="#">Services</a>
-  <a href="#">Clients</a>
-  <a href="#">Contact</a>
-</div>
-
-<div id="main">
-  <h2>Sidenav Push Example</h2>
-  <p>Click on the element below to open the side navigation menu, and push this content to the right.</p>
-  <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; open</span>
-</div>
-</div>  
+<p> Dett är hubben</p>
 </template>
 
 <script lang="ts">
 import App from 'App.vue';
-import Vue from 'Vue';
-
+import { Component, Prop, Vue } from 'vue-property-decorator';
 @Component({
   components: {
-
+    
   }
 })
 export default class Hub extends Vue {
 }
-
-
 
 </script>
 
@@ -38,47 +22,4 @@ body {
   font-family: "Lato", sans-serif;
 }
 
-.sidenav {
-  height: 100%;
-  width: 0;
-  position: fixed;
-  z-index: 1;
-  top: 0;
-  left: 0;
-  background-color: #111;
-  overflow-x: hidden;
-  transition: 0.5s;
-  padding-top: 60px;
-}
-
-.sidenav a {
-  padding: 8px 8px 8px 32px;
-  text-decoration: none;
-  font-size: 25px;
-  color: #818181;
-  display: block;
-  transition: 0.3s;
-}
-
-.sidenav a:hover {
-  color: #f1f1f1;
-}
-
-.sidenav .closebtn {
-  position: absolute;
-  top: 0;
-  right: 25px;
-  font-size: 36px;
-  margin-left: 50px;
-}
-
-#main {
-  transition: margin-left .5s;
-  padding: 16px;
-}
-
-@media screen and (max-height: 450px) {
-  .sidenav {padding-top: 15px;}
-  .sidenav a {font-size: 18px;}
-}
 </style>
